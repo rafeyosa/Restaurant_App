9c0c0a1d617dede6458604a8ad54ea722f671df0
Rafeyosa <rafeyosa@gmail.com>
2022-07-13 00:10:44 +0700

    Add Notification Helper

---
+++ lib/helper/notification_helper.dart
@@ -0,0 +1,93 @@
+import 'dart:convert';
+
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+import 'package:rxdart/rxdart.dart';
+import 'package:rxdart/subjects.dart';
+
+import '../data/models/restaurant.dart';
+import '../utils/navigation.dart';
+
+final selectNotificationSubject = BehaviorSubject<String>();
+
+class NotificationHelper {
+  static NotificationHelper? _instance;
+
+  NotificationHelper._internal() {
+    _instance = this;
+  }
+
+  factory NotificationHelper() => _instance ?? NotificationHelper._internal();
+
+  Future<void> initNotifications(
+      FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin) async {
+    var initializationSettingsAndroid =
+        const AndroidInitializationSettings('app_icon');
+
+    var initializationSettingsIOS = const IOSInitializationSettings(
+      requestAlertPermission: false,
+      requestBadgePermission: false,
+      requestSoundPermission: false,
+    );
+
+    var initializationSettings = InitializationSettings(
+      android: initializationSettingsAndroid,
+      iOS: initializationSettingsIOS,
+    );
+
+    await flutterLocalNotificationsPlugin.initialize(
+      initializationSettings,
+      onSelectNotification: (String? payload) async {
+        if (payload != null) {
+          print('notification payload: ' + payload);
+        }
+        selectNotificationSubject.add(payload ?? 'empty payload');
+      },
+    );
+  }
+
+  Future<void> showNotification(
+      FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin,
+      Restaurant restaurant) async {
+    var channelId = '1';
+    var channelName = 'channel_01';
+    var channelDescription = 'restaurant app channel';
+
+    var androidPlatformChannelSpecifics = AndroidNotificationDetails(
+      channelId,
+      channelName,
+      channelDescription: channelDescription,
+      importance: Importance.max,
+      priority: Priority.high,
+      ticker: 'ticker',
+      styleInformation: const DefaultStyleInformation(true, true),
+    );
+
+    var iOSPlatformChannelSpecifics = const IOSNotificationDetails();
+
+    var platformChannelSpecifics = NotificationDetails(
+      android: androidPlatformChannelSpecifics,
+      iOS: iOSPlatformChannelSpecifics,
+    );
+
+    var titleNotification = "<b>Popular Restaurant</b>";
+    var titleName = restaurant.name;
+
+    await flutterLocalNotificationsPlugin.show(
+      0,
+      titleNotification,
+      titleName,
+      platformChannelSpecifics,
+      payload: json.encode(restaurant.toJson()),
+    );
+  }
+
+  void configureSelectNotificationSubject(String route) {
+    selectNotificationSubject.stream.listen(
+      (String payload) async {
+        var data = Restaurant.fromJson(json.decode(payload));
+        var article = data;
+        Navigation.intentWithData(route, article.id);
+      },
+    );
+  }
+}
